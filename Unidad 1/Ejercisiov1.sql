3f1ee88614fd1c2d3edf49fc52a3c2e326234b99
Jonathan Emmanuel <canedojonathan8@gmail.com>
2024-01-17 16:02:20 -0600

    primer comit ejercisio 1 terminado

---
+++ Unidad 1/Ejercisiov1.sql
@@ -0,0 +1,37 @@
+-- SQL- LDD
+-- Crear la base de datos bdentornosvip
+
+CREATE DATABASE bdentornosvip
+go
+-- Cambiar de base de datos 
+use bdentornosvip
+go
+--Crea tabla categoria
+Create table tblcategoria(
+    categoriaId int not null,
+	descripcion varchar(100) not null,  
+	constraint  pk_tblcategoria
+	primary key (categoriaId),
+	constraint unique_descripcion
+	unique (descripcion)
+)
+go
+
+CREATE TABLE tblProducto(
+  productoId int not null,
+  nombreP varchar(50) not null,
+  precio decimal(10,2) not null,
+  existencia int not null,
+  categoriaId int not null,
+  constraint pk_tblProducto
+  primary key(productoId),
+  constraint unique_nombreP
+  unique(nombreP),
+  constraint chk_precio
+  check(precio>0.0 and precio<=10000),
+  constraint chk_existencia
+  check(existencia>=0),
+  constraint fk_tblProducto_tblcategoria
+  foreign key(categoriaId)
+  references tblcategoria(categoriaId)
+)
---
+++ Unidad 1/Practica1.sql
@@ -0,0 +1,15 @@
+--Pratica 1
+CREATE DATABASE Practica1
+go
+
+use Practica1
+go
+
+create table contactoid(
+contactoid int not null,
+nombre varchar (50) not null,
+constraint  pk_contactoid
+primary key (contactoId)
+)
+
+go
